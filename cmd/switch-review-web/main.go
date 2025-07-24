903460ddd09e8a9b1b583d0f30b7024f9b04b261
mtanng9 <mlt152498@gmail.com>
2025-07-24 17:11:08 -0400

    added start of index handler func

--- cmd/switch-review-web/main.go
+++ cmd/switch-review-web/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/mtanng9/switch-review/server"
 )
 
 func main() {
-	fmt.Print("Hello World")
 	server.StartServer()
 }
--- server/server.go
+++ server/server.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is index")
+}
+
 func StartServer() {
+	http.HandleFunc("/", index)
 	log.Println("Starting the server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
