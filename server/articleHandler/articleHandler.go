4edf83ad9f32515ce99da1a03c0e88ee5b97a1f6
mtanng9 <mlt152498@gmail.com>
2025-07-27 13:45:04 -0400

    aded 404 page for non existent articles

--- server/articleHandler/articleHandler.go
+++ server/articleHandler/articleHandler.go
@@ -2,8 +2,8 @@ package articlehandler
 
 import (
 	"database/sql"
+	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"path"
 	"time"
@@ -20,15 +20,19 @@ type Article struct {
 func GetArticle(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
+		baseFile := path.Join("html", "base.html")
 
 		var article Article
 		query := `SELECT id, title, snippet, body, created_at FROM articles WHERE id = ?`
 		err := db.QueryRow(query, id).Scan(&article.Id, &article.Title, &article.Snippet, &article.Body, &article.CreatedAt)
 		if err != nil {
-			log.Fatalf("Could not get Article: %v", err)
+			fmt.Printf("Could not get Article: %v \n", err)
+			notFoundFile := path.Join("html", "notFound.html")
+			t, _ := template.ParseFiles(baseFile, notFoundFile)
+			t.Execute(w, nil)
+			return
 		}
 
-		baseFile := path.Join("html", "base.html")
 		articleFile := path.Join("html", "article.html")
 		t, _ := template.ParseFiles(baseFile, articleFile)
 		t.Execute(w, article)
--- server/reviewHandler/reviewHandler.go
+++ server/reviewHandler/reviewHandler.go
@@ -2,7 +2,7 @@ package reviewhandler
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 	"net/http"
 	"path"
 	"text/template"
@@ -27,15 +27,19 @@ type Review struct {
 func GetReview(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
+		baseFile := path.Join("html", "base.html")
 
 		var review Review
 		query := `SELECT id, title, snippet, score, body, created_at FROM reviews WHERE id = ?`
 		err := db.QueryRow(query, id).Scan(&review.Id, &review.Title, &review.Snippet, &review.Score, &review.Body, &review.createdAt)
 		if err != nil {
-			log.Fatalf("Could not get Review: %v", err)
+			fmt.Printf("Could not get Review: %v \n", err)
+			notFoundFile := path.Join("html", "notFound.html")
+			t, _ := template.ParseFiles(baseFile, notFoundFile)
+			t.Execute(w, nil)
+			return
 		}
 
-		baseFile := path.Join("html", "base.html")
 		reviewFile := path.Join("html", "review.html")
 		t, _ := template.ParseFiles(baseFile, reviewFile)
 		t.Execute(w, review)
